5d866dca34c12ba0fad98d65f04fb53c402ee3ee
SwarajBaral <baralswaraj40@gmail.com>
2021-06-30 15:56:41 +0530

    UI changes

--- src/Components/Dashboard/Numbers.js
+++ src/Components/Dashboard/Numbers.js
@@ -12,156 +12,110 @@ const tilt_percent = 0.47;
 const gauge_percent = 0.15;
 
 var batteryConfig = {
-    percent: 0.5,
-    outline: {
-        border: 4,
-        distance: 4,
-    },
-    wave: { length: 128 },
+  percent: 0.5,
+  outline: {
+    border: 4,
+    distance: 4,
+  },
+  wave: { length: 128 },
 };
 const dataSource = [
-    {
-        key: 1,
-        aid: "AexkiXKXJSDF092",
-        remark: "In transit",
-    },
-    {
-        key: 2,
-        aid: "AmanKDJSLKKL032",
-        remark: "In transit",
-    },
+  {
+    key: 1,
+    aid: "AexkiXKXJSDF092",
+    remark: "In transit",
+  },
+  {
+    key: 2,
+    aid: "AmanKDJSLKKL032",
+    remark: "In transit",
+  },
 ];
 
 const columns = [
-    {
-        title: "Asset ID",
-        dataIndex: "aid",
-        key: "aid",
-    },
-    {
-        title: "Remark",
-        dataIndex: "remark",
-        key: "remark",
-    },
+  {
+    title: "Asset ID",
+    dataIndex: "aid",
+    key: "aid",
+  },
+  {
+    title: "Remark",
+    dataIndex: "remark",
+    key: "remark",
+  },
 ];
 
 export default function MyComponent(props) {
-    const today = new Date();
+  const today = new Date();
 
-    const [date, setDate] = useState(today.toLocaleString());
-    const [time, setTime] = useState(
-        today.toLocaleString("en-US", {
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            hour12: true,
+  const [date, setDate] = useState(today.toLocaleString());
+  const [time, setTime] = useState(
+    today.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+    })
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(
+        new Date().toLocaleString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          hour12: true,
         })
-    );
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(
-                new Date().toLocaleString("en-US", {
-                    hour: "numeric",
-                    minute: "numeric",
-                    second: "numeric",
-                    hour12: true,
-                })
-            );
-            setDate(new Date().toLocaleString());
-            console.log(date, time);
-        }, 1000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-    return (
-        <div>
-            <h1>Shipment Status</h1>
-            <p>{date}</p>
-
-<<<<<<< HEAD
-            <Row>
-                <Col span={12}>
-                    <Card title={Card_title} style={{ margin: 5 }}>
-                        <center>{Card_content}</center>
-                        <center>Asset ID : {Asset_number}</center>
-                        <small>Time Updated : {time}</small>
-                    </Card>
-                    <br />
-                    <Row>
-                        <Col span={12} style={{ padding: 5 }}>
-                            <Card title="Shock">
-                                <GaugeChart
-                                    id="gauge-chart3"
-                                    nrOfLevels={8}
-                                    percent={gauge_percent}
-                                    formatTextValue={(gauge_percent) =>
-                                        (gauge_percent / 12.5).toFixed(1) + " G"
-                                    }
-                                    textColor="#464A4F"
-                                />
-                                <center>Force(G)</center>
-                                <center>
-                                    <small>Time Updated: {time}</small>
-                                </center>
-                            </Card><br />
-                            <Card title="Temperature">
-                                <Temperature value={temp_value} height={300} />
-                                <center>
-                                    Temperature - <b>{temp_value} °C</b>
-                                </center>
-                                <center>
-                                    <small>Time Updated: {time}</small>
-                                </center>
-                            </Card>
-                        </Col>
-                        <Col span={12} style={{ padding: 5 }}>
-                            <Card title="Orientation">
-                                <GaugeChart
-                                    id="gauge-chart2"
-                                    nrOfLevels={30}
-                                    percent={tilt_percent}
-                                    formatTextValue={(tilt_percent) =>
-                                        (tilt_percent * 1.8).toFixed(1) + " deg"
-                                    }
-                                    cornerRadius={3}
-                                    textColor="#464A4F"
-                                />
-                                <center>Tilt(Deg)</center>
-                                <center>
-                                    <small>Time Updated: {time}</small>
-                                </center>
-                            </Card>
-                            <br />
-                            <Card title="Battery" className="battery">
-                                <div style={{ textAlign: "center" }}>
-                                    <Liquid {...batteryConfig} />
-                                </div>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col span={12}>
-                    <center>
-                        <h1>Asset Details</h1>
-                    </center>
-                    <Table dataSource={dataSource} columns={columns} />
-                </Col>
-            </Row>
-        </div>
-    );
-=======
-              <div className="battery-sensor" style={{ textAlign: "center" }}>
-                <Liquid {...batteryConfig} />
-                <center>Sensor Battery</center>
+      );
+      setDate(new Date().toLocaleString());
+      console.log(date, time);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div>
+      <h1>Shipment Status</h1>
+      <p>{date}</p>
+      <Row>
+        <Col span={12}>
+          <Card title={Card_title} style={{ margin: 5 }}>
+            <center>{Card_content}</center>
+            <center>Asset ID : {Asset_number}</center>
+            <small>Time Updated : {time}</small>
+          </Card>
+          <br />
+          <Row>
+            <Col span={12} style={{ padding: 5 }}>
+              <Card title="Shock">
+                <GaugeChart
+                  id="gauge-chart3"
+                  nrOfLevels={8}
+                  percent={gauge_percent}
+                  formatTextValue={(gauge_percent) =>
+                    (gauge_percent / 12.5).toFixed(1) + " G"
+                  }
+                  textColor="#464A4F"
+                />
+                <center>Force(G)</center>
                 <center>
                   <small>Time Updated: {time}</small>
                 </center>
-              </div>
+              </Card>
+              <br />
+              <Card title="Temperature">
+                <Temperature value={temp_value} height={300} />
+                <center>
+                  Temperature - <b>{temp_value} °C</b>
+                </center>
+                <center>
+                  <small>Time Updated: {time}</small>
+                </center>
+              </Card>
             </Col>
-            {/* <Col span={1}></Col> */}
-            <Col span={11}>
+            <Col span={12} style={{ padding: 5 }}>
               <Card title="Orientation">
                 <GaugeChart
                   id="gauge-chart2"
@@ -179,26 +133,21 @@ export default function MyComponent(props) {
                 </center>
               </Card>
               <br />
-              <Card title="Shock">
-                <GaugeChart
-                  id="gauge-chart3"
-                  nrOfLevels={8}
-                  percent={gauge_percent}
-                  formatTextValue={(gauge_percent) =>
-                    (gauge_percent / 12.5).toFixed(1) + " G"
-                  }
-                  textColor="#464A4F"
-                />
-                <center>Force(G)</center>
-                <center>
-                  <small>Time Updated: {time}</small>
-                </center>
+              <Card title="Battery" className="battery">
+                <div style={{ textAlign: "center" }}>
+                  <Liquid {...batteryConfig} />
+                </div>
               </Card>
             </Col>
           </Row>
         </Col>
+        <Col span={12}>
+          <center>
+            <h1>Asset Details</h1>
+          </center>
+          <Table dataSource={dataSource} columns={columns} />
+        </Col>
       </Row>
     </div>
   );
->>>>>>> master
 }
